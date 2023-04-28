dd30584b7fe2e05f0a8df7e222705b17a99ffc88
Igor Nascimento <igor.rdn7@gmail.com>
2023-04-28 20:19:50 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,40 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales {get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime date, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Date = date;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord rs)
+        {
+            Sales.Remove(rs);
+        }
+
+        public double TotalSales(DateTime intial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= intial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
