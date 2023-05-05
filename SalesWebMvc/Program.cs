a56f4f2125b9ceb6533cdaa87d3fbacf10e1cdfe
Igor Nascimento <igor.rdn7@gmail.com>
2023-05-05 20:29:18 -0300

    Sellerservice and basic FindAll

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
-//using SalesWebMvc.Services;
+using SalesWebMvc.Services;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,7 +21,7 @@ var connectionStringMysql = builder.Configuration.GetConnectionString("SalesWebM
 builder.Services.AddDbContext<SalesWebMvcContext>(options => options.UseMySql(connectionStringMysql, ServerVersion.Parse("8.0.25-mysql")));
 
 builder.Services.AddScoped<SeedingService>();
-//builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<SellerService>();
 //builder.Services.AddScoped<DepartmentService>();
 //builder.Services.AddScoped<SalesRecordService>();
 
@@ -36,10 +36,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-//Abaixo exemplo de como rodar a injeção de dependência, no caso o SeedService adicionado acima: "builder.Services.AddScoped<SeedingService>();",
-//no Startup do programa. Apesar de ter uma lógica para não repopular o banco, vou deixar aqui comentado pra
-//não ficar rodando sempre que executar o programa.
 
 app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
 
